39bbff03fcb0de39159b7bc0d3c0a9f372ab7d1a
Katsushun89 <Katsushun89@gmail.com>
2020-07-23 00:19:47 +0900

    Modify teleop_joy.launch to add joy node.

--- launch/teleop_joy.launch.py
+++ launch/teleop_joy.launch.py
@@ -7,36 +7,70 @@ import launch_ros.actions
 from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
+from launch.actions import EmitEvent
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 import lifecycle_msgs.msg
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    ld = launch.LaunchDescription()
+    ld = LaunchDescription()
 
-    config_param_dir = launch.substitutions.LaunchConfiguration(
-        'config_param_dir',
+    joy_config = LaunchConfiguration('joy_config')
+    joy_dev = LaunchConfiguration('joy_dev')
+    #config_filepath = LaunchConfiguration('config_filepath')
+
+    declare_joy_config = DeclareLaunchArgument('joy_config', default_value='gamepad')
+
+    declare_joy_dev = DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0')
+
+    declare_joy_config_filepath = DeclareLaunchArgument('joy_config_filepath', default_value=[
+            launch.substitutions.TextSubstitution(text=os.path.join(
+                get_package_share_directory('cyberpunk_cleaner'), 'config', '')),
+            joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')])
+
+#    joy_config_filepath = LaunchConfiguration(
+#        'joy_config_filepath',
+#        default=os.path.join(
+#            get_package_share_directory('cyberpunk_cleaner'),
+#            'config',
+#            'gamepad.config.yaml'))
+
+    joy_node = Node(
+        package='joy',
+        node_executable='joy_node',
+        parameters=[{'dev': joy_dev,
+                    'deadzone': 0.3,
+                    'autorepeat_rate': 20.0,}])
+
+    teleop_twist_joy_node = Node(
+            package='teleop_twist_joy', node_executable='teleop_node',
+            name='teleop_twist_joy_node', parameters=[LaunchConfiguration('joy_config_filepath')])
+
+    ca_config_filepath = LaunchConfiguration(
+        'ca_config_filepath',
         default=os.path.join(
-            get_package_share_directory('ca_driver'),
+            get_package_share_directory('cyberpunk_cleaner'),
             'config',
-            'default.yaml'))
+            'ca_driver.default.yaml'))
 
-    ca_driver = launch_ros.actions.LifecycleNode(
+    ca_driver = LifecycleNode(
         node_name='ca_driver',
         package='ca_driver',
         node_executable='ca_driver',
-        parameters=[config_param_dir],
+        parameters=[ca_config_filepath],
         output='screen')
 
-    to_inactive = launch.actions.EmitEvent(
+    to_inactive = EmitEvent(
         event=launch_ros.events.lifecycle.ChangeState(
             lifecycle_node_matcher=launch.events.matches_action(ca_driver),
             transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
         )
     )
 
-    to_inactive = launch.actions.EmitEvent(
+    to_inactive = EmitEvent(
         event=launch_ros.events.lifecycle.ChangeState(
             lifecycle_node_matcher=launch.events.matches_action(ca_driver),
             transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
@@ -71,6 +105,12 @@ def generate_launch_description():
             ],
         )
     )
+
+    ld.add_action(declare_joy_dev)
+    ld.add_action(declare_joy_config)
+    ld.add_action(declare_joy_config_filepath)
+    ld.add_action(joy_node)
+    ld.add_action(teleop_twist_joy_node)
 
     ld.add_action(from_unconfigured_to_inactive)
     ld.add_action(from_inactive_to_active)
